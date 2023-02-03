fc7b95ff73a03d173d82b42fd60d04796c69136d
bbo-git <73382903+bbo-git@users.noreply.github.com>
2023-02-03 08:50:26 +0100

    redesign: added scroll view to first time dialog view

--- Sources/EduIDExpansion/Controllers/Onboarding/FirstTimeDialogViewController.swift
+++ Sources/EduIDExpansion/Controllers/Onboarding/FirstTimeDialogViewController.swift
@@ -6,11 +6,15 @@ class FirstTimeDialogViewController: OnBoardingBaseViewController, ScreenWithScr
     //MARK: - screen type
     var screenType: ScreenType = .firstTimeDialogScreen
     
+    
     //MARK: - primary button
     let addButton = EduIDButton(type: .primary, buttonTitle: "Connect your school/institution")
     
-    //MARK: - stck
+    //MARK: - stack
     var stack: AnimatedVStackView!
+    
+    //MARK: - scroll view
+    var scrollView = UIScrollView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,9 +30,23 @@ class FirstTimeDialogViewController: OnBoardingBaseViewController, ScreenWithScr
         screenType.configureNavigationItem(item: navigationItem, target: self, action: #selector(goBack))
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        scrollView.contentSize = stack.frame.inset(by: UIEdgeInsets(top: -24, left: -24, bottom: -24, right: -24)).size
+    }
+    
     func setupUI() {
+        //MARK: - scroll view setup
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(scrollView)
+        scrollView.edges(to: view)
+        
         //MARK: - postertext
+        let posterParent = UIView()
         let posterLabel = UILabel.posterTextLabelBicolor(text: "Are you studying in NL?\nConnect your institution!", size: 24, primary: "Connect your institution!")
+        posterParent.addSubview(posterLabel)
+        posterLabel.edges(to: posterParent)
         
         //MARK: - textView
         let paragraph = NSMutableParagraphStyle()
@@ -49,32 +67,36 @@ You must therefore add the following information to your eduID:
         let spaceView = UIView()
         
         //MARK: - additional text view
+        let labelParent = UIView()
         let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
         label.numberOfLines = 0
         let attributedText2 = NSMutableAttributedString(string: "Add this information by connecting your school/institution via SURFconext.", attributes: [.foregroundColor: UIColor.charcoalColor, .font: UIFont.sourceSansProRegular(size: 16)])
         attributedText2.setAttributeTo(part: "Add this information", attributes: [.font: UIFont.sourceSansProBold(size: 16)])
         label.attributedText = attributedText2
+        labelParent.addSubview(label)
+        label.edges(to: labelParent)
         
         //MARK: - skip button
         let skipButton = EduIDButton(type: .ghost, buttonTitle: "Skip this")
         skipButton.isEnabled = false
         
         //MARK: - create the stackview
-        stack = AnimatedVStackView(arrangedSubviews: [posterLabel, textView, spaceView, label, addButton, skipButton])
+        stack = AnimatedVStackView(arrangedSubviews: [posterParent, textView, spaceView, labelParent, addButton, skipButton])
         stack.axis = .vertical
         stack.translatesAutoresizingMaskIntoConstraints = false
         stack.distribution = .fill
         stack.alignment = .center
         stack.spacing = 20
-        view.addSubview(stack)
+        scrollView.addSubview(stack)
         
         //MARK: - add constraints
-        stack.edgesToSuperview(insets: TinyEdgeInsets(top: 24, left: 24, bottom: 24, right: 24), usingSafeArea: true)
+        stack.edges(to: scrollView, insets: TinyEdgeInsets(top: 24, left: 24, bottom: -24, right: -24))
+        stack.width(to: scrollView, offset: -48)
         textView.width(to: stack)
         posterLabel.width(to: stack)
         addButton.width(to: stack, offset: -24)
         skipButton.width(to: stack, offset: -24)
-        posterLabel.height(68)
         
         stack.hideAndTriggerAll(onlyThese: [4, 5])
         
