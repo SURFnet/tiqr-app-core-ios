d8a41d346c8f77bdfddf3a0b4437f42a74299b36
Yasser Farahi <72069280+risinggalaxy@users.noreply.github.com>
2023-04-06 07:31:19 +0200

    EDURED-54: The input doesn't follow regex

--- Sources/EduIDExpansion/Utilities/Constants.swift
+++ Sources/EduIDExpansion/Utilities/Constants.swift
@@ -1,4 +1,5 @@
 //
+<<<<<<< HEAD
 //  File.swift
 //  
 //
@@ -6,3 +7,23 @@
 //
 
 import Foundation
+=======
+//  Constants.swift
+//  
+//
+//  Created by Yasser Farahi on 06/04/2023.
+//
+
+import Foundation
+
+enum Constants {
+    
+    enum RegEx {
+        static let emailRegex = #"^(?=.{6,})[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"#
+        static let nameRegex = #"^[a-zA-Z' ]+$"#
+        static let passwordRegex = #"(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).{8,}"#
+        static let phoneRegex = #"^(?=[+0])[+\d\s]*(?:\d\s*){10,}$"#
+    }
+    
+}
+>>>>>>> 7946759 (EDURED-54: The input doesn't follow regex)
