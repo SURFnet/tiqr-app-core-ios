77162a3202ff584ef24b046eed80e55723b0da0d
Yasser Farahi <72069280+risinggalaxy@users.noreply.github.com>
2023-04-06 08:36:33 +0200

    Improved regex for accepting names and phone numbers

--- Sources/EduIDExpansion/Utilities/Constants.swift
+++ Sources/EduIDExpansion/Utilities/Constants.swift
@@ -11,9 +11,9 @@ enum Constants {
     
     enum RegEx {
         static let emailRegex = #"^(?=.{6,})[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"#
-        static let nameRegex = #"^[a-zA-Z' ]+$"#
+        static let nameRegex = #"^[a-zA-Z]+(?:-[a-zA-Z]+)*$"#
         static let passwordRegex = #"(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).{8,}"#
-        static let phoneRegex = #"^(?=[+0])[+\d\s]*(?:\d\s*){10,}$"#
+        static let phoneRegex = #"^(?=[+0\d])(?:\+?\d{1,2}\s?)?\(?(?:\d{3}[\s-]?\d{3}[\s-]?\d{4}|\d{10})\)?$"#
     }
     
     enum InvalidInput {
