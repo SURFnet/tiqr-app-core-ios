05e5c417012a058971edb7b5ba40298c9937ae5c
Yasser Farahi <72069280+risinggalaxy@users.noreply.github.com>
2023-05-08 13:39:55 +0200

    parsing challange from QR

--- Sources/EduIDExpansion/Flows/CreateEduID/ViewModels/CreatePincodeAndBiometricAccessViewModel.swift
+++ Sources/EduIDExpansion/Flows/CreateEduID/ViewModels/CreatePincodeAndBiometricAccessViewModel.swift
@@ -38,7 +38,7 @@ final class CreatePincodeAndBiometricAccessViewModel: NSObject {
     func verifyPinSimilarity() {
         if firstEnteredPin == secondEnteredPin {
             Task {
-                await requestTiqrEnroll(withBiometrics: false) { [weak self] success in
+                await requestTiqrEnroll() { [weak self] success in
                     guard let self else { return }
                     if success {
                         self.showUseBiometricScreenClosure?()
@@ -74,32 +74,43 @@ final class CreatePincodeAndBiometricAccessViewModel: NSObject {
     
     //run After the second pin
     @MainActor
-    func requestTiqrEnroll(withBiometrics: Bool, completion: @escaping ((Bool) -> Void)) {
-        Task {
-            do{
-                let enrolment = try await TiqrControllerAPI.startEnrollmentWithRequestBuilder()
-                    .addHeader(name: Constants.Headers.authorization, value: keychain.getString(for: Constants.KeyChain.accessToken) ?? "")
-                    .execute()
-                    .body
-                
-                ServiceContainer.sharedInstance().challengeService.startChallenge(fromScanResult: enrolment.url ?? "") { [weak self] type, object, error in
-                    guard let self else { return }
-                    self.secondEnteredPin.removeLast(2)
-                    ServiceContainer.sharedInstance().challengeService.complete(object as! EnrollmentChallenge, usingBiometricID: withBiometrics, withPIN: self.pinToString(pinArray: self.secondEnteredPin)) { success, error in
-                        if success {
-                            self.enrollmentChallenge = (object as? EnrollmentChallenge)
-                            completion(true)
-                        } else {
-                            completion(false)
-                        }
+    func requestTiqrEnroll(completion: @escaping ((Bool) -> Void)) {
+        if enrollmentChallenge == nil {
+            Task {
+                do{
+                    let enrolment = try await TiqrControllerAPI.startEnrollmentWithRequestBuilder()
+                        .addHeader(name: Constants.Headers.authorization, value: keychain.getString(for: Constants.KeyChain.accessToken) ?? "")
+                        .execute()
+                        .body
+                    
+                    ServiceContainer.sharedInstance().challengeService.startChallenge(fromScanResult: enrolment.url ?? "") { [weak self] type, object, error in
+                        guard let self else { return }
+                        self.secondEnteredPin.removeLast(2)
+                        self.createIdentity(for: object as? EnrollmentChallenge, completion: completion)
                     }
+                } catch let error as NSError {
+                    dump(error)
+                    completion(false)
                 }
-            } catch let error as NSError {
-                dump(error)
-                completion(false)
+            }
+        } else {
+            self.createIdentity(for: self.enrollmentChallenge, completion: completion)
+        }
+    }
+    
+    private func createIdentity(for challange: EnrollmentChallenge?, completion: @escaping ((Bool) -> Void)) {
+        if let enrolChallange = challange {
+            self.secondEnteredPin.removeLast(2)
+            ServiceContainer.sharedInstance().challengeService.complete(enrolChallange, usingBiometricID: false, withPIN: self.pinToString(pinArray: self.secondEnteredPin)) { success, error in
+                if success {
+                    completion(true)
+                } else {
+                    completion(false)
+                }
             }
         }
     }
+
 }
 
 extension CreatePincodeAndBiometricAccessViewModel {
